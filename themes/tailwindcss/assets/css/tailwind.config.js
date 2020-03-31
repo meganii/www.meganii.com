1ad41031df929e59a5e491ac1554535321faacae
meganii <yuhei24@gmail.com>
2020-03-31 19:50:51 +0900

    Add tailwindcss theme

---
+++ themes/tailwindcss/assets/css/tailwind.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      ja: [
+        'Avenir',
+        'Helvetica Neue',
+        'Helvetica',
+        'Arial',
+        'Hiragino Sans',
+        'ヒラギノ角ゴシック',
+        'メイリオ',
+        'Meiryo',
+        'YuGothic',
+        'Yu Gothic',
+        'ＭＳ Ｐゴシック',
+        'MS PGothic',
+        'sans-serif'
+      ]
+    },
+    listStyleType:{
+      none: 'none',
+      disc: 'disc',
+      decimal: 'decimal',
+      circle: 'circle',
+    },
+    extend: {
+      colors: {
+        gray: {
+          '100': '#f8f8f8',
+          '200': '#eeeeee',
+          '300': '#e0e0e0',
+          '400': '#bdbdbd',
+          '500': '#9e9e9e',
+          '600': '#757575',
+          '700': '#616161',
+          '800': '#424242',
+          '900': '#212121',
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
