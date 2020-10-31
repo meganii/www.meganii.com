610bb170742c29313bf6714368ff5c147f1e47e5
meganii <meganii@outlook.jp>
2020-10-31 22:55:03 +0900

    Fix #20

--- src/app.ts
+++ src/app.ts
@@ -1,64 +1,64 @@
 import * as express from 'express';
-import * as client from 'cheerio-httpcli';
+import * as cheerio from 'cheerio';
+import fetch from 'node-fetch';
+
 import * as requestImageSize from 'request-image-size';
 
 const app = express();
 
-app.get("/size", (expressRequest, expressResponse, expressNext) => {
+app.get("/size", async (expressRequest, expressResponse, expressNext) => {
     const url = expressRequest.query.url;
-    client.fetch(url, (err, $, res, body) => {
-        if (err) {
-            expressNext(err)
-            return;
-        }
 
-        requestImageSize(url)
-            .then(size => {
-                const result = {
-                    width: size.width,
-                    height: size.height,
-                };
-                expressResponse.json(result);
-            })
-            .catch(err => console.error(err));
-    });
+    requestImageSize(url)
+    .then(size => {
+        const result = {
+            width: size.width,
+            height: size.height,
+        };
+        expressResponse.json(result);
+    })
+    .catch(err => console.error(err));
 })
 
-app.get("/getogp", (expressRequest, expressResponse, expressNext) => {
+app.get("/getogp", async (expressRequest, expressResponse, expressNext) => {
     const url = expressRequest.query.url;
-    client.fetch(url, (err, $, res, body) => {
-        if (err) {
-            expressNext(err)
-            return;
-        }
 
-        const result = {
-            exists: false,
-            title: "",
-            description: "",
-            url: "",
-            image: "",
-            site_name: "",
-            type: "",
-        }
+    const res = await fetch(url);
 
-        const ogTitleQuery = $("meta[property='og:title']");
+    if (res.status !== 200) {
+        console.log(`error status:${res.status}`);
+        expressNext(res.status);
+        return;
+    }
 
-        if (ogTitleQuery.length > 0) {
-            result.exists = true;
-            result.title = $("meta[property='og:title']").attr("content");
-            result.description = $("meta[property='og:description']").attr("content");
-            result.url = $("meta[property='og:url']").attr("content");
-            result.image = $("meta[property='og:image']").attr("content");
-            result.site_name = $("meta[property='og:site_name']").attr("content");
-            result.type = $("meta[property='og:type']").attr("content");
-        } else {
-            result.title = $("head title").text()
-            result.description = $("meta[name='description']").attr("content");
-        }
+    const result = {
+        exists: false,
+        title: "",
+        description: "",
+        url: "",
+        image: "",
+        site_name: "",
+        type: "",
+    }
 
-        expressResponse.json(result);
-    });
+      // jqueryチックに使えるように変換
+    const $ = cheerio.load(await res.text());
+    const ogTitleQuery = $("meta[property='og:title']");
+
+    if (ogTitleQuery.length > 0) {
+        result.exists = true;
+        result.title = $("meta[property='og:title']").attr("content");
+        result.description = $("meta[property='og:description']").attr("content");
+        result.url = $("meta[property='og:url']").attr("content");
+        result.image = $("meta[property='og:image']").attr("content");
+        result.site_name = $("meta[property='og:site_name']").attr("content");
+        result.type = $("meta[property='og:type']").attr("content");
+    } else {
+        result.title = $("head title").text()
+        result.description = $("meta[name='description']").attr("content");
+    }
+
+    expressResponse.json(result);
 
 })
 
