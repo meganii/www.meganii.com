1c2dad07f5164d4da7a5b58155aeab87f88aa11e
meganii <yuhei24@gmail.com>
2020-11-07 14:23:13 +0900

    Refactoring

--- themes/tailwindcss/assets/css/postcss.config.js
+++ themes/tailwindcss/assets/css/postcss.config.js
@@ -4,11 +4,6 @@ module.exports = {
     plugins: [        
         require('postcss-import')({path: [themeDir]}), 
         require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'), 
-        require('autoprefixer')({
-            'browsers': [
-                'last 2 versions',
-            ],
-        }),
-        require('postcss-reporter'),
+        require('autoprefixer'),
     ]
 }
