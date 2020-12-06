52d02a3c7fd7d16285bb674ccecf8cf225f17bd9
meganii <meganii@outlook.jp>
2020-12-06 16:12:01 +0900

    Add rakuten yahoo link

--- script/update_all_amazon_asin.js
+++ script/update_all_amazon_asin.js
@@ -2,9 +2,10 @@
 const Apaw = require('@johnfoderaro/apaw');
 const fs = require('fs').promises;
 const path = require("path");
+const fetch = require('node-fetch');
 
 const regAmazonWord = /\{\{\% amazon.+?(([A-Z]|[0-9]){10}) \%\}\}/g;
-const mdDir = '/content/blog';
+const mdDir = 'content/blog';
 const dataDir = 'data/amazon';
 
 const apaw = Apaw({
@@ -16,6 +17,46 @@ const apaw = Apaw({
 
 const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
 
+const rakutenEndPoint = `https://app.rakuten.co.jp/services/api/Product/Search/20170426?format=json&applicationId=${process.env['RAKUTEN_APP_ID']}`;
+const getRakutenAffiliateLink = async (eanList) => {
+  let data = {};
+  for (janCode of eanList) {
+    const res = await fetch(`${rakutenEndPoint}&keyword=${janCode}`);
+    const jsonData = await res.json();
+    if (jsonData['Products']) {
+      if (jsonData['Products'][0]) {
+        const product = jsonData['Products'][0]['Product'];
+        data = {
+          'productUrlPC': product['productUrlPC'],	
+          'productUrlMobile': product['productUrlMobile']
+        }
+      }
+    }
+    break;
+  }
+  return data;
+};
+
+const affiliateId = 'https%3A%2F%2Fck.jp.ap.valuecommerce.com%2Fservlet%2Freferral%3Fsid%3D3067752%26pid%3D886865716%26vc_url%3D';
+const yahooEndpoint = `https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch?appid=${process.env['YAHOO_APP_ID']}`;
+const getYahooAffiliateLink = async (eanList) => {
+  let data = {};
+  for (janCode of eanList) {
+    const url = `${yahooEndpoint}&affiliate_type=vc&affiliate_id=${affiliateId}&jan_code=${janCode}`;
+    console.log(url);
+    const res = await fetch(url);
+    const jsonData = await res.json();
+    if (jsonData['totalResultsAvailable']  === 0){
+      continue;
+    }
+    const productUrl = `https://shopping.yahoo.co.jp/search?p=${janCode}`;
+    data = {
+      "productUrl": `https://ck.jp.ap.valuecommerce.com/servlet/referral?sid=3067752&pid=886865716&vc_url=${encodeURIComponent(productUrl)}`
+    }
+    break;
+  }
+  return data;
+};
 
 (async () => {
   const files = await fs.readdir(mdDir);
@@ -46,7 +87,8 @@ const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
           'ItemInfo.ByLineInfo',
           'ItemInfo.Classifications',
           'ItemInfo.ContentInfo',
-          'ItemInfo.Title'
+          'ItemInfo.Title',
+          'ItemInfo.ExternalIds'
         ],
       });
       const data = JSON.parse(results.data);
@@ -63,6 +105,19 @@ const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
           }
           item['ItemInfo']['ContentInfo']['PublicationDate']['DisplayValue'] = displayDate;
         }
+
+        let eanList = [];
+        if ('ExternalIds' in item['ItemInfo'] && 'EANs' in item['ItemInfo']['ExternalIds']) {
+          eanList = item['ItemInfo']['ExternalIds']['EANs']['DisplayValues'];
+        }
+        if (eanList) {
+          const rakutenAffiliateLink = await getRakutenAffiliateLink(eanList);
+          item['Rakuten'] = rakutenAffiliateLink;
+  
+          const yahooAffiliateLink = await getYahooAffiliateLink(eanList);
+          item['Yahoo'] = yahooAffiliateLink;
+        }
+
         await fs.writeFile(`${dataDir}/${item['ASIN']}.json`, JSON.stringify(item, null, '    '));
       }
 
