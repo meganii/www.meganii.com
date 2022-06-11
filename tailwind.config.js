e42c9146c81c137b8e47488d53348fae08af4509
meganii <meganii@outlook.jp>
2022-06-12 08:35:40 +0900

    Upgrade tailwindcssv3

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+themeDir = 
+module.exports = {
+  content: [
+    'themes/tailwindcss/layouts/**/*.html',
+    'layouts/**/*.html',
+    'content/**/*.html',
+    'content/**/*.md',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
