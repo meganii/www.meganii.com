eb63864d54ba50e61974cc18bd6bbea9d9751a87
meganii <meganii@outlook.jp>
2023-07-16 21:47:56 +0900

    Update

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
+let tailwindConfig = process.env.HUGO_FILE_TAILWIND_CONFIG_JS || './tailwind.config.js';
+const tailwind = require('tailwindcss')(tailwindConfig);
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
-    plugins: {
-        'postcss-import': {},
-        'tailwindcss/nesting': {},
-        tailwindcss: {},
-    }
-}
+	// eslint-disable-next-line no-process-env
+	plugins: [tailwind, ...(process.env.HUGO_ENVIRONMENT === 'production' ? [autoprefixer] : [])],
+};
