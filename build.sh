78f1507462a77c0b6590d4ca388245d9a0d9f3cc
meganii <meganii@outlook.jp>
2025-11-17 08:07:23 +0900

    change log revel

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 # !/bin/bash
 export GOPRIVATE=github.com/meganii/vault-content,github.com/meganii/affiliate-data,github.com/meganii/amazon-product-data,github.com/meganii/tweet-data
 git config --global url."https://${GO_MODULE_TOKEN_MEGANII_COM}:@github.com/".insteadOf "https://github.com/"
-hugo mod clean
 hugo mod graph
-hugo build --logLevel debug
+hugo build --logLevel info
 npx -y pagefind --site public 
