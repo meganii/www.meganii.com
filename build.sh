bbffdc1c726ccedfae822eaa065ac918a9168f49
meganii <meganii@outlook.jp>
2025-11-17 19:44:56 +0900

    update

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 export GOPRIVATE=github.com/meganii/vault-content,github.com/meganii/affiliate-data,github.com/meganii/amazon-product-data,github.com/meganii/tweet-data
 git config --global url."https://${GO_MODULE_TOKEN_MEGANII_COM}:@github.com/".insteadOf "https://github.com/"
 hugo mod graph
+go version
 hugo build --logLevel info
 npx -y pagefind --site public 
